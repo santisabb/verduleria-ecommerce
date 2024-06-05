a74ff20897c470c48e1c085507a0d59262a78ab8
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-06-05 16:16:34 -0300

    header estatico(sin funcionalidad)  completado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,14 @@
+import NavBar from "./NavBar.jsx"
+import ShopCard from "./ShopCard.jsx"
+import Logo from "./Logo.jsx"
+
 export function Header(){
     return(
         <>
         <header className="header">
-            <div className="logo">
-                <img src="https://th.bing.com/th/id/OIG3.2fzho3I0UskKl3j9l4pZ?pid=ImgGn" alt="" className="logo-img"/>
-                <h1>Verduleria</h1>
-            </div>
-            <nav className="nav-bar">
-                <ul className="menu">
-                    <li className="menu-item">
-                        <a href="#">opcion1</a>
-                    </li>
-                    <li className="menu-item">
-                        <a href="#">opcion1</a>
-                    </li>
-                    <li className="menu-item">
-                        <a href="#">opcion1</a>
-                    </li>
-                    <li className="menu-item">
-                        <a href="#">opcion1</a>
-                    </li>
-                </ul>
-                <div className="open-menu">
-                    <i className="bi bi-list"></i>
-                </div>
-                <div className="shop-card">
-                    <i className="bi bi-cart-fill"></i>
-                </div>
-            </nav>
+            <Logo></Logo>
+            <ShopCard></ShopCard>
+            <NavBar></NavBar>
         </header>
         </>
     )
---
+++ src/components/ShopCard.jsx
@@ -0,0 +1,9 @@
+function ShopCard(){
+    return(
+        <div className="shop-card">
+            <i className="bi bi-cart-fill"></i>
+        </div>
+    )
+}
+
+export default ShopCard
