6fce56fe6fc8050cd964fc655c9d3a0d4c50d3a0
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-06-06 01:25:59 -0300

    agrego seccion de productos destacados con un card-container y una seccion de categorisa usando display grid

---
+++ src/components/Categories.jsx
@@ -0,0 +1,26 @@
+function Categories(){
+    return(
+        <>
+        <section className="categories">
+            <div className="cat cat1">
+                <span className="cat-name">Frutas de la tierra</span>
+                <button>Ver</button>
+            </div>
+            <div className="cat cat2">
+                <span className="cat-name">Frutas de la finca</span>
+                <button>Ver</button>
+            </div>
+            <div className="cat cat3">
+                <span className="cat-name">Frutos secos</span>
+                <button>Ver</button>
+            </div>
+            <div className="cat cat4">
+                <span className="cat-name">Otros productos</span>
+                <button>Ver</button>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Categories
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import Carousel from "./Carousel.jsx"
+import FeaturedProducts from "./FeaturesProducts.jsx"
+import Categories from "./Categories.jsx"
 
 function Home(){
     return(
         <>
         <Carousel></Carousel>
+        <FeaturedProducts></FeaturedProducts>
+        <Categories></Categories>
         </>
     )
 }
