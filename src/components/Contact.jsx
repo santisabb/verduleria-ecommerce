bb53bd017ceced31960beef09eaff4c6789c4240
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-06-12 15:18:44 -0300

    agrego estilos a la pagina de contacto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,24 @@
 function Contact (){
     return (
         <>
-        <div className="page-info">
-            <h2>Contactaté con nosotros</h2>
-            <p>Podes ponerte en contacto con nosotros para cualquier consulta, posibles proyectos
+        <div className="contact-page">
+            <div className="page-info">
+                <h2>Contactaté con nosotros</h2>
+                <p>Podes ponerte en contacto con nosotros para cualquier consulta, posibles proyectos
             y asociaciones comerciales
-            </p>
-        </div>
-        <div className="contact-options">
-            <div className="wpp">
-                <i className="bi bi-phone"></i>
-                <h3>Escribinos</h3>
-                <p>WhatsApp: 123456789</p>
+                </p>
             </div>
-            <div className="email">
-                <i className="bi bi-envelope-at"></i>
-                <h3>Nos podes mandar un mail</h3>
-                <p>unmail@gmail.com</p>
+            <div className="contact-options">
+                <div className="wpp">
+                    <i className="bi bi-phone"></i>
+                    <h3>Escribinos</h3>
+                    <p>WhatsApp: 123456789</p>
+                </div>
+                <div className="email">
+                    <i className="bi bi-envelope-at"></i>
+                    <h3>Nuestro mail</h3>
+                    <p>unmail@gmail.com</p>
+                </div>
             </div>
         </div>
         </>
