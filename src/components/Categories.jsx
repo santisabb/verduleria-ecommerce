013d45b85c2a8334817fe517aceb861a41db3c34
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-06-13 14:42:56 -0300

    actualizo estilos de login

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,22 +1,24 @@
+import { Link } from "react-router-dom"
+
 function Categories(){
     return(
         <>
         <section className="categories">
             <div className="cat cat1">
                 <span className="cat-name">Frutas de la tierra</span>
-                <button type="button" className="btn btn-info">Ver</button>
+                <Link to={'#'} className="btn btn-info">Ver</Link>
             </div>
             <div className="cat cat2">
                 <span className="cat-name">Frutas de la finca</span>
-                <button type="button" className="btn btn-info">Ver</button>
+                <Link to={'#'} className="btn btn-info">Ver</Link>
             </div>
             <div className="cat cat3">
                 <span className="cat-name">Frutos secos</span>
-                <button type="button" className="btn btn-info">Ver</button>
+                <Link to={'#'} className="btn btn-info">Ver</Link>
             </div>
             <div className="cat cat4">
                 <span className="cat-name">Otros productos</span>
-                <button type="button" className="btn btn-info">Ver</button>
+                <Link to={'#'} className="btn btn-info">Ver</Link>
             </div>
         </section>
         </>
