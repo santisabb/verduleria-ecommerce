674f79b98671873043fd55505958150c129d278f
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-06-27 01:53:12 -0300

    agrego estilos en vista mobile para la pagina del carrito y le doy una estructura basica en vista desktop

--- src/components/ShopCart.jsx
+++ src/components/ShopCart.jsx
@@ -5,22 +5,37 @@ import Button from './Button.jsx'
 function ShopCart(){
     return(
         <>
-        <h2>Carrito de pedidos</h2>
-        <div className="product-container">
-            <ShopCartProduct img={'https://i.pinimg.com/564x/b2/c0/e8/b2c0e8cdc61331e141f624842cdd319a.jpg'} name={'Nombre del producto'} price={'$1313'}></ShopCartProduct>
-            <ShopCartProduct img={'https://i.pinimg.com/564x/b2/c0/e8/b2c0e8cdc61331e141f624842cdd319a.jpg'} name={'Nombre del producto'} price={'$1313'}></ShopCartProduct>
-            <ShopCartProduct img={'https://i.pinimg.com/564x/b2/c0/e8/b2c0e8cdc61331e141f624842cdd319a.jpg'} name={'Nombre del producto'} price={'$1313'}></ShopCartProduct>
-            <ShopCartProduct img={'https://i.pinimg.com/564x/b2/c0/e8/b2c0e8cdc61331e141f624842cdd319a.jpg'} name={'Nombre del producto'} price={'$1313'}></ShopCartProduct>
-            
+        <div className="shop-cart">
+            <h2 className="page-title">Carrito de pedidos</h2>
+            <div className="product-container">
+                <ShopCartProduct img={'https://i.pinimg.com/564x/b2/c0/e8/b2c0e8cdc61331e141f624842cdd319a.jpg'} name={'Nombre del producto'} price={'$1313'}></ShopCartProduct>
+                <ShopCartProduct img={'https://i.pinimg.com/564x/b2/c0/e8/b2c0e8cdc61331e141f624842cdd319a.jpg'} name={'Nombre del producto'} price={'$1313'}></ShopCartProduct>
+                <ShopCartProduct img={'https://i.pinimg.com/564x/b2/c0/e8/b2c0e8cdc61331e141f624842cdd319a.jpg'} name={'Nombre del producto'} price={'$1313'}></ShopCartProduct>
+                <ShopCartProduct img={'https://i.pinimg.com/564x/b2/c0/e8/b2c0e8cdc61331e141f624842cdd319a.jpg'} name={'Nombre del producto'} price={'$1313'}></ShopCartProduct>        
+            </div>
+            <div className="subtotal">
+                <div className="final-price">
+                    <span className="title-price">Subtotal:</span>
+                    <span className="price">$1313</span>
+                </div>
+                <div className="options">
+                    <span className="options-title">Seleccione una opcion:</span>
+                    <div className="forms">
+                        <div className="form-check form">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+                            <label className="form-check-label" htmlFor="flexRadioDefault1">Retiro por local</label>
+                        </div>
+                        <div className="form-check form">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
+                            <label className="form-check-label" htmlFor="flexRadioDefault2">Envío a domicilio</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="d-grid gap-2 col-6 mx-auto">
+                    <Button style={"btn btn-primary"}>FINALIZAR COMPRA</Button>
+                </div>
+            </div>
         </div>
-        <div className="subtotal">
-            <span>Subtotal:$13312</span>
-            <div className="d-grid gap-2 col-6 mx-auto">
-            <Button style={"btn btn-primary"}>Button</Button>
-            <Button style={"btn btn-primary"}>Button</Button>
-        </div>
-        </div>
-        
         </>
     )
 }
