326749b21eb14bcdd54163d3808401a7e06ceef4
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-06-29 16:39:19 -0300

    modifico estilo de las fuentes de texto en las cartas de los productos; le doy ruta a los links de la seccion de categorias, cambios realizados en la pagina home

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,19 +6,19 @@ function Categories(){
         <section className="categories">
             <div className="cat cat1">
                 <span className="cat-name">Frutas de la tierra</span>
-                <Link to={'#'} className="btn btn-info">Ver</Link>
+                <Link to={'/store'} className="btn btn-info">Ver</Link>
             </div>
             <div className="cat cat2">
                 <span className="cat-name">Frutas de la finca</span>
-                <Link to={'#'} className="btn btn-info">Ver</Link>
+                <Link to={'/store'} className="btn btn-info">Ver</Link>
             </div>
             <div className="cat cat3">
                 <span className="cat-name">Frutos secos</span>
-                <Link to={'#'} className="btn btn-info">Ver</Link>
+                <Link to={'/store'} className="btn btn-info">Ver</Link>
             </div>
             <div className="cat cat4">
                 <span className="cat-name">Otros productos</span>
-                <Link to={'#'} className="btn btn-info">Ver</Link>
+                <Link to={'/store'} className="btn btn-info">Ver</Link>
             </div>
         </section>
         </>
