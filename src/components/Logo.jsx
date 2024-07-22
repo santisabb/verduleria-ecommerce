50422c2c3f048a3b6a2249d3a96a67628ecd6bd0
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2024-07-22 14:47:42 -0300

    modifico codigo de estilos en partials de header y login con la intencion de limpiar el codigo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom"
+
 function Logo(){
     return(
-        <div className="logo">
-            <img src="https://th.bing.com/th/id/OIG3.2fzho3I0UskKl3j9l4pZ?pid=ImgGn" alt="" className="logo-img"/>
-            <h1 className="name">Verduleria</h1>
-        </div>
+        <>
+        <Link to="/">
+            <div className="logo">         
+                <img src="https://th.bing.com/th/id/OIG3.2fzho3I0UskKl3j9l4pZ?pid=ImgGn" alt="" className="logo-img"/>
+                <h1 className="name">Verduleria</h1>
+            </div>
+        </Link>
+        </>
     )
 }
 
